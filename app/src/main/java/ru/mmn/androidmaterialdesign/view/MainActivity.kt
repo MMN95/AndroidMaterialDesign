fd49d5d393cc366b31e23d3c75a1e425c08de092
Michael Makulov <mmn95@mail.ru>
2021-09-08 21:41:22 +0500

    -Добавлен класс CustomImageView для отображения фото в равных пропорциях -Создан макет фрагмента, отображающего фото дня

--- app/src/main/java/ru/mmn/androidmaterialdesign/view/MainActivity.kt
+++ app/src/main/java/ru/mmn/androidmaterialdesign/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.mmn.androidmaterialdesign
+package ru.mmn.androidmaterialdesign.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.mmn.androidmaterialdesign.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
