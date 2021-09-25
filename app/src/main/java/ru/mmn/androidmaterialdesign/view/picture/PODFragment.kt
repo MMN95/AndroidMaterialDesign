e535f9933692461a63336e0a791df87db316ae36
Michael Makulov <mmn95@mail.ru>
2021-09-25 21:30:20 +0500

    -Учтены замечания ко 2-му уроку (добавлено изменение темы на светлую или темную)

--- app/src/main/java/ru/mmn/androidmaterialdesign/view/picture/PODFragment.kt
+++ app/src/main/java/ru/mmn/androidmaterialdesign/view/picture/PODFragment.kt
@@ -5,14 +5,12 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import coil.api.load
 import com.google.android.material.bottomappbar.BottomAppBar
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 import ru.mmn.androidmaterialdesign.R
 import ru.mmn.androidmaterialdesign.databinding.FragmentMainBinding
 import ru.mmn.androidmaterialdesign.view.MainActivity
@@ -21,8 +19,6 @@ import ru.mmn.androidmaterialdesign.viewmodel.PODData
 import ru.mmn.androidmaterialdesign.viewmodel.PODViewModel
 
 class PODFragment : Fragment() {
-
-    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
     private var _binding: FragmentMainBinding? = null
     private val binding: FragmentMainBinding
         get() {
